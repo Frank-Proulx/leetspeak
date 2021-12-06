39e19c6175b2cf79287c7ad208ed4b46d132acee
Frank-Proulx <frankproulx7@gmail.com>
2021-12-06 14:05:57 -0800

    add 3 rule after passing test
    Co-authored-by: Marisa Edgar <marisaedgar1212@gmail.com>

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,3 +1,12 @@
 def leetspeak(string)
-  string
+  word_array = string.split("")
+  new_word_array = []
+  word_array.each do |letter|
+    if letter == "e"
+      new_word_array.push("3")
+    else
+      new_word_array.push(letter)
+    end
+  end
+  new_word_array.join("")
 end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -6,4 +6,9 @@ describe('#leetspeak') do
     expect(leetspeak("happy")).to(eq("happy"))
   end
 
+  it('replaces every "e" in a string with a "3"') do
+    expect(leetspeak("elephant")).to(eq("3l3phant"))
+  end
+
+  
 end
