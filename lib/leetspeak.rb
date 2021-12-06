6d158875ff6c35b4af1595c3e7a2a19406fbd59c
Frank-Proulx <frankproulx7@gmail.com>
2021-12-06 14:31:06 -0800

    add z rule and modify so can be used for multi word strings
    Co-authored-by: Marisa Edgar <marisaedgar1212@gmail.com>

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,16 +1,25 @@
 def leetspeak(string)
-  word_array = string.split("")
-  new_word_array = []
-  word_array.each do |letter|
-    if letter == "e"
-      new_word_array.push("3")
-    elsif letter == "o"
-      new_word_array.push("0")
-    elsif letter == "I"
-      new_word_array.push("1")
-    else
-      new_word_array.push(letter)
+  sentence_array = string.split
+  new_sentence_array = []
+  sentence_array.each do |word|
+    word_array = word.split("")
+    new_word_array = []
+    counter = 0
+    word_array.each do |letter|
+      if letter == "e"
+        new_word_array.push("3")
+      elsif letter == "o"
+        new_word_array.push("0")
+      elsif letter == "I"
+        new_word_array.push("1")
+      elsif (letter == "s") && (counter > 0)
+        new_word_array.push("z")
+      else
+        new_word_array.push(letter)
+      end
+      counter += 1
     end
+    new_sentence_array.push(new_word_array.join(""))
   end
-  new_word_array.join("")
+  new_sentence_array.join(" ")
 end
